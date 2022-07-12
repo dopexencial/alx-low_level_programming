ea21e658b71819b617eb22d9d6a72fec4fd6ae35
Samuel Lateef <dope.sencial@gmail.com>
2022-07-12 16:37:35 -0700

    mnb

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,28 +2,12 @@
 /**
   * _puts - prints a string using putchar
   * @str: the string
-  * Author: Chimezie Onwudiwe
-  * Return: void
-  */
-void _puts(char *str)
-{
-#include "main.h"
-/**
-  * _puts - prints a string using putchar
-  * @str: the string
-  * Author: Chimezie Onwudiwe
+  * Author: Samuel Lateef
   * Return: void
   */
 void _puts(char *str)
 {
 	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
-}	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
